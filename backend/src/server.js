7e5afed147c82c8d5efd37464600dee40fbd7332
lamiaa <lamiaa3105@gmail.com>
2025-12-02 02:37:23 +0100

    checking

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,3 +8,4 @@ app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
 console.log("DATABASE_URL:", process.env.DATABASE_URL);
+console.log("JWT_SECRET:", process.env.JWT_SECRET); 
